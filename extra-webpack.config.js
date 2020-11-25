a00e26ae7a65c3a3b72910e15bb2d63c42d6c7ea
maxceem <maxceem@gmail.com>
2020-11-25 23:36:08 +0200

    cross import demo
    e2e tests are NOT supported

--- extra-webpack.config.js
+++ extra-webpack.config.js
@@ -3,6 +3,13 @@ const singleSpaAngularWebpack = require('single-spa-angular/lib/webpack').defaul
 module.exports = (config, options) => {
   const singleSpaWebpackConfig = singleSpaAngularWebpack(config, options);
 
+  // we have to list here all the microapps which we would like to use in imports
+  // so webpack doesn't tries to import them
+  singleSpaWebpackConfig.externals = {
+    '@topcoder/micro-frontends-navbar-app':
+      '@topcoder/micro-frontends-navbar-app',
+  };
+
   // Feel free to modify this webpack config however you'd like to
   return singleSpaWebpackConfig;
 };
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EmptyRouteComponent } from './empty-route/empty-route.component';
+import { AuthDemoComponent } from './auth-demo/auth-demo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EmptyRouteComponent
+    EmptyRouteComponent,
+    AuthDemoComponent
   ],
   imports: [
     BrowserModule,
