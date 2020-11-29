1c95021af1c3f39cb4717b84bcae6279cdc30a0c
maxceem <maxceem@gmail.com>
2020-11-29 15:55:08 +0200

    feat: No Sidebar Demo

---
+++ src/app/no-sidebar-demo/no-sidebar-demo.component.ts
@@ -0,0 +1,26 @@
+/**
+ * This component demonstrates how we can disable sidebar for some subroutes.
+ *
+ * For example this component disables sidebar for routes "/micro-frontends-angular-route/no-sidebar/*".
+ */
+import { Component, OnInit } from '@angular/core';
+import {
+  disableSidebarForRoute,
+  enableSidebarForRoute,
+} from '@topcoder/micro-frontends-navbar-app';
+
+const COMPONENT_ROUTE = "/micro-frontends-angular-route/no-sidebar/*";
+
+@Component({
+  selector: 'tc-ex-no-sidebar-demo',
+  templateUrl: './no-sidebar-demo.component.html',
+})
+export class NoSidebarDemoComponent implements OnInit {
+  route = COMPONENT_ROUTE
+  enable = () => enableSidebarForRoute(COMPONENT_ROUTE)
+  disable = () => disableSidebarForRoute(COMPONENT_ROUTE)
+
+  ngOnInit(): void {
+    disableSidebarForRoute(COMPONENT_ROUTE)
+  }
+}
--- src/typings.d.ts
+++ src/typings.d.ts
@@ -4,6 +4,8 @@ declare module '@topcoder/micro-frontends-navbar-app' {
   export const setAppMenu: any;
   export const getAuthUserTokens: any;
   export const getAuthUserProfile: any;
+  export const disableSidebarForRoute: any;
+  export const enableSidebarForRoute: any;
 }
 
 declare module "*.svg" {
